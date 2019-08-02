b93cf8c06f5269ba0c300da120744788c028cdb7
ncfrey <ncfrey@users.noreply.github.com>
2019-08-02 11:54:47 -0700

    Cleaning up

--- pymatgen/analysis/topology/__init__.py
+++
@@ -1,7 +0,0 @@
-# coding: utf-8
-# Copyright (c) Pymatgen Development Team.
-# Distributed under the terms of the MIT License.
-
-"""
-The topology package implements tools to analyze band topology.
-"""
